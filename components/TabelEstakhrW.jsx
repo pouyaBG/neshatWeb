1a45527cb2201bebfda6a57ff594d31ffb934c72
pouya <b_pooya@yahoo.com>
2023-12-05 22:42:06 +0330

    chnage

--- components/TabelEstakhrW.jsx
+++ components/TabelEstakhrW.jsx
@@ -1,57 +1,23 @@
+import axios from "axios";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const TabelEstakhrW = () => {
-  const ListItem = [
-    {
-      day: "یکشنبه",
-      date: "10 آبان",
-      man: false,
-      start: "10:00",
-      end: "22:00",
-      price1: "700.000",
-      price2: "1.200.000",
-      quntitiy: "150",
-      buy: true,
-    },
-
-    {
-      day: "سه شنبه",
-      date: "11 آبان",
-      man: false,
-      start: "10:00",
-      end: "16:00",
-      price1: "700.000",
-      price2: "1.200.000",
-      quntitiy: "150",
-      buy: true,
-    },
-    {
-      day: "پنجشنبه",
-      date: "13 آبان",
-      man: false,
-      start: "10:00",
-      end: "22:00",
-      price1: "700.000",
-      price2: "1.200.000",
-      quntitiy: "10",
-      buy: true,
-    },
-    {
-      day: "جمعه",
-      date: "14 آبان",
-      man: false,
-      start: "10:00",
-      end: "16:00",
-      price1: "700.000",
-      price2: "1.200.000",
-      quntitiy: "10",
-      buy: false,
-    },
-  ];
+  const [ListItem, setListItem] = useState();
+  useEffect(() => {
+    axios
+      .get("https://django-sport.iran.liara.run/api/secure/ticketshop_get/", {})
+      .then((response) => {
+        setListItem(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   const router = useRouter();
   const [checkboxStates, setCheckboxStates] = useState(
-    ListItem.map(() => false)
+    ListItem?.map(() => false)
   );
 
   const handleCheckboxChange = (index) => {
@@ -85,68 +51,70 @@ const TabelEstakhrW = () => {
     <section className="w-full rounded-[15px] ">
       <div className="flex flex-col items-center  ">
         <div className="w-full mt-5 flex flex-col items-center justify-center gap-5 mb-5  bg-white">
-          {ListItem?.map((item, index) => (
-            <div
-              key={index}
-              className="w-full flex justify-center gap-5 items-center gap-y-5"
-            >
+          {ListItem?.filter((item) => item.gender === "F").map(
+            (item, index) => (
               <div
-                className={` w-[104.055px] flex flex-col items-center justify-center text-white ${
-                  item.man ? "bg-[#00A1FF]" : "bg-[#FF089A]"
-                } px-4 py-2 rounded-[8.754px]`}
+                key={index}
+                className="w-full flex justify-center gap-5 items-center gap-y-5"
               >
-                <p>{item.day}</p>
-                <p>{item.date}</p>
-              </div>
-              <div className=" py-2 rounded-[8.754px] flex items-center justify-center border ">
-                <div className="border-l px-10 flex flex-col items-center justify-center">
-                  <p className="font-thin">شروع</p>
-                  <p>{item.start}</p>
+                <div
+                  className={` w-[104.055px] flex flex-col items-center justify-center text-[#FFFFFF] ${
+                    item.gender === "M" ? "bg-[#00A1FF]" : "bg-[#FF089A]"
+                  } px-4 py-2 rounded-[8.754px]`}
+                >
+                  <p>{item.day_name}</p>
+                  <p>{item.date}</p>
                 </div>
-                <div className=" px-10 flex flex-col items-center justify-center">
-                  <p className="font-thin">پایان</p>
-                  <p>{item.end}</p>
+                <div className=" py-2 rounded-[8.754px] flex items-center justify-center border ">
+                  <div className="border-l px-10 flex flex-col items-center justify-center">
+                    <p className="font-thin">شروع</p>
+                    <p>{item.start_time}</p>
+                  </div>
+                  <div className=" px-10 flex flex-col items-center justify-center">
+                    <p className="font-thin">پایان</p>
+                    <p>{item.end_time}</p>
+                  </div>
                 </div>
-              </div>
-              {item.buy ? (
-                <div className="border px-[175px] rounded-[8.754px] py-[19px]">
-                  <p>ظـــرفــیـــت تــکــمــیــل</p>
-                </div>
-              ) : (
-                <>
-                  <div className=" py-2 rounded-[8.754px] flex items-center justify-center border ">
-                    <div className="border-l px-10 flex flex-col items-center justify-center">
-                      <p className="font-thin">قیمت (هر نفر)</p>
-                      <p>{item.price1} ریال</p>
-                    </div>
-                    <div className=" px-10 flex flex-col items-center justify-center">
-                      <div className="flex items-center gap-2">
-                        <input
-                          type="checkbox"
-                          checked={checkboxStates[index]}
-                          onChange={() => handleCheckboxChange(index)}
-                        />
-                        <p className="font-thin">به همراه ماساژ</p>
+                {item.quantity === 0 ? (
+                  <div className="border px-[175px] rounded-[8.754px] py-[19px]">
+                    <p>ظـــرفــیـــت تــکــمــیــل</p>
+                  </div>
+                ) : (
+                  <>
+                    <div className=" py-2 rounded-[8.754px] flex items-center justify-center border ">
+                      <div className="border-l px-10 flex flex-col items-center justify-center">
+                        <p className="font-thin">قیمت (هر نفر)</p>
+                        <p>{item.Pool_price} ریال</p>
                       </div>
-                      <p>{item.price2} ریال</p>
+                      <div className=" px-10 flex flex-col items-center justify-center">
+                        <div className="flex items-center gap-2">
+                          <input
+                            type="checkbox"
+                            // checked={checkboxStates[index]}
+                            onChange={() => handleCheckboxChange(index)}
+                          />
+                          <p className="font-thin">به همراه ماساژ</p>
+                        </div>
+                        <p>{item.Massage_price} ریال</p>
+                      </div>
                     </div>
-                  </div>
-                  <div className=" px-4 py-2 rounded-[8.754px] border flex items-center flex-col justify-between ">
-                    <p className="font-thin">ظرفیت</p>
-                    <p>{item.quntitiy}</p>
-                  </div>
-                </>
-              )}
-              <div
-                className={`cursor-pointer px-20 py-[19px] rounded-[8.754px] ${
-                  item.buy ? "bg-[#9D9D9D]" : "bg-[#59AC49]"
-                }`}
-                onClick={() => handleBuyClick(index)}
-              >
-                <button className="text-white">خرید</button>
+                    <div className=" px-4 py-2 rounded-[8.754px] border flex items-center flex-col justify-between ">
+                      <p className="font-thin">ظرفیت</p>
+                      <p>{item.quantity}</p>
+                    </div>
+                  </>
+                )}
+                <div
+                  className={`cursor-pointer px-20 py-[19px] rounded-[8.754px] ${
+                    item.buy ? "bg-[#9D9D9D]" : "bg-[#59AC49]"
+                  }`}
+                  onClick={() => handleBuyClick(index)}
+                >
+                  <button className="text-white">خرید</button>
+                </div>
               </div>
-            </div>
-          ))}
+            )
+          )}
         </div>
       </div>
     </section>
