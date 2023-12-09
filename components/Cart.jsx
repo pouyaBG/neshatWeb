41d30188c89f6fd3c8f492d565abb3615e7a32bb
pouya <b_pooya@yahoo.com>
2023-12-09 23:04:23 +0330

    c

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -5,38 +5,73 @@ import Image from "next/image";
 
 const Cart = () => {
   const { selectedProducts } = useCart();
+  const totalPrice = selectedProducts.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0
+  );
   console.log(selectedProducts);
   return (
-    <div className="container p-10 w-full ">
+    <div className="container p-10 w-full m-auto">
       <div className="border-b-2 border-[#2594FF] w-fit pb-1">
         <h1 className="text-black  text-[25px] not-italic font-bold leading-6">
           سبد خرید
         </h1>
       </div>
-      <div>
+      <div className="flex w-full gap-5 justify-between items-start">
         {selectedProducts?.length > 0 ? (
-          <div>
+          <div className="w-full">
             {selectedProducts.map((product) => (
-              <div key={product.id} className="border-b-2 p-2">
-                <div className="flex items-center gap-2 p-5">
-                  <div className="w-[118.006px] h-[168px]">
-                    <Image src={product.img} width={500} height={500} />
+              <div key={product.id} className="  border-b-2 p-1">
+                <div className="flex items-start gap-2 p-5">
+                  <div className="w-[160px] h-[200px]">
+                    <Image src={product.img} width={700} height={700} />
                   </div>
-                  <div>
-                    <p>{product.name}</p>
-                    <p>{product.price}</p>
-                    <p>{product.quantity}</p>
+                  <div className="flex flex-col gap-3">
+                    <p className="text-black text-[23.723px] not-italic font-medium leading-[normal]">
+                      {product.name}
+                    </p>
+                    <p className="text-[#656565] text-right text-[15px] not-italic font-medium leading-6">
+                      {product.quantity} مجاز به خرید
+                    </p>
+                    <p className="text-[#656565] text-right text-[15px] not-italic font-medium leading-6">
+                      موجود در انبار
+                    </p>
+                    <p className="text-black text-[20.723px] not-italic font-medium leading-[normal]">
+                      {product.price} تومان
+                    </p>
                   </div>
                 </div>
               </div>
             ))}
           </div>
         ) : (
-          <p>سبد خرید شما خالی است.</p>
+          <div className="w-full m-auto">
+            <p>سبد خرید شما خالی است.</p>
+          </div>
         )}
-      </div>
-      <div>
-        <Link href="/checkout">پرداخت</Link>
+        <div className="bg-white w-[336px]  shadow-[0px_0px_6px_1px_rgba(37,124,255,0.15)] rounded-[15px] p-7 mt-5">
+          <div className="flex justify-between w-full">
+            <p className="text-neutral-500 text-center text-base not-italic font-medium leading-6">
+              قیمت کالا ها ({selectedProducts.length})
+            </p>
+            <p className="text-neutral-500 text-center text-base not-italic font-medium leading-6">
+              {totalPrice} تومان
+            </p>
+          </div>
+          <div className="flex justify-between w-full mt-5">
+            <p className="text-neutral-500 text-center text-base not-italic font-medium leading-6">
+              جمع کل سبد خرید
+            </p>
+            <p className="text-neutral-500 text-center text-base not-italic font-medium leading-6">
+              {totalPrice} تومان
+            </p>
+          </div>
+          <div className="w-full bg-[#59AC49] rounded-[8px] mt-5">
+            <Link href="#">
+              <p className=" p-2 text-center text-white">پرداخت</p>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
