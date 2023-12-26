c72dbaeffc1e3899430c3549189ab47936e62367
pouya <b_pooya@yahoo.com>
2023-12-26 18:09:24 +0330

    c

--- app/(main)/gallery/page.jsx
+++ app/(main)/gallery/page.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <div className="relative mb-10">
+        <div className="absolute  -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4 z-50">
+          <p className="text-white text-[40px] not-italic font-bold leading-[normal] text-center mb-2">
+            گالری مجموعه
+          </p>
+          <p className=" text-white text-center text-[22px] not-italic font-normal leading-[normal]">
+            گردآوری لحظات بی‌نظیر تیم، تمرینات ایستاده در زمین بازی، و
+            جشنواره‌های شگفت‌انگیز در گالری ورزشی ما، جهت به اشتراک گذاری شور و
+            انگیزه ورزشی با شما.
+          </p>
+        </div>
+        <img
+          src="/img/Rectangle 34624459.png"
+          alt=""
+          className="w-full absolute left-0 bottom-0 object-cover"
+        />
+        <img src="/img/Mask group.png" alt="" className="w-full object-cover" />
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
